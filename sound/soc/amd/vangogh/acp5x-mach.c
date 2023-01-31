d76484ff1380e036da87d347b269cdee428854e5
Bretta <bretta@valvesoftware.com>
2023-01-31 15:55:06 -0800

    fixed private_data to asoc_substream

--- sound/soc/amd/vangogh/acp5x-mach.c
+++ sound/soc/amd/vangogh/acp5x-mach.c
@@ -174,7 +174,7 @@ static int acp5x_nau8821_hw_params(struct snd_pcm_substream *substream,
 static int acp5x_max98388_startup(struct snd_pcm_substream *substream)
 {
 	struct snd_pcm_runtime *runtime = substream->runtime;
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(substream);
 	struct snd_soc_card *card = rtd->card;
 	struct acp5x_platform_info *machine = snd_soc_card_get_drvdata(card);
 
@@ -191,7 +191,7 @@ static int acp5x_max98388_startup(struct snd_pcm_substream *substream)
 static int acp5x_max98388_hw_params(struct snd_pcm_substream *substream,
 				    struct snd_pcm_hw_params *params)
 {
-	struct snd_soc_pcm_runtime *rtd = substream->private_data;
+	struct snd_soc_pcm_runtime *rtd = asoc_substream_to_rtd(substream);
 	struct snd_soc_card *card = rtd->card;
 	struct snd_soc_dai *codec_dai =
 			snd_soc_card_get_codec_dai(card,
